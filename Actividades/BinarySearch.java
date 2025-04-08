9ba7efcb35efedf0a95d210d4ddc5d2fb26280f0
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-04-08 04:04:02 -0500

    v9

--- Actividades/BinarySearch.java
+++ Actividades/BinarySearch.java
@@ -1,3 +1,9 @@
 public class BinarySearch {
-
-}
+    int binarySearch(int arr[], int lo, int hi, int x) { 
+        if (hi >= lo && lo < arr.length - 1) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == x)
+            return mid;
+            if (arr[mid] > x)
+            
+    }
