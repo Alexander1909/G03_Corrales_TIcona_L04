2dba2d055fac887f003d70a3b6664e79bc240d7c
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-04-08 04:55:06 -0500

    v33

--- Ejercicios/SubconjuntoConRestricciones.java
+++ Ejercicios/SubconjuntoConRestricciones.java
@@ -3,4 +3,16 @@ public class SubconjuntoConRestricciones {
     public static boolean esPotenciaDe2(int num) {
         return num > 0 && (num & (num - 1)) == 0;
     }
+    public static boolean puedeSumar(int[] arr, int index, int objetivo) {
+        if (index == arr.length) {
+            return objetivo == 0;
+        }
+
+        int actual = arr[index];
+
+        // Regla 1: si es potencia de 2, debe incluirse
+        if (esPotenciaDe2(actual)) {
+            return puedeSumar(arr, index + 1, objetivo - actual);
+        }
+        
 }
