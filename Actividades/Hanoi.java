fb6a1a329b9e2f9aae27b546297ca3ef010a43e2
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-04-07 22:28:16 -0500

    v3

--- Actividades/Hanoi.java
+++ Actividades/Hanoi.java
@@ -7,7 +7,18 @@ public class Hanoi {
     }
 
     //Creando el metodo recursivo para la solucion de las torres hanoi //
-    
-
-
+    public void torresHanoi(int discos, int torre1, int torre2, int torre3){
+        //Caso base
+        if (discos == 1){
+        System.out.println("Mover disco de torre " + torre1 + " a torre " + torre3);
+        } else{
+        //Dominio (problema – 1)
+        torresHanoi(discos -1, torre1, torre3,torre2);
+        System.out.println("mover disco de torre "+ torre1 + " a torre " + torre3);
+        torresHanoi(discos -1, torre2, torre1, torre3);
+        }
+    }
 }
+
+
+
