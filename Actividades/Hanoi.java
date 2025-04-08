0ae546ef63127d13d7e8bbae52805058b9200952
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-04-07 22:25:28 -0500

    v2

--- Actividades/Hanoi.java
+++ Actividades/Hanoi.java
@@ -1,3 +1,13 @@
 public class Hanoi {
 
+    public static void main(String[] args) {
+        torresHanoi(3,2,1,3);
+
+
+    }
+
+    //Creando el metodo recursivo para la solucion de las torres hanoi //
+    
+
+
 }
