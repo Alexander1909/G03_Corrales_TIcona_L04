0e0f3fc4d5575593f5aea5a2aeda5f3f5bebc252
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-04-08 05:29:43 -0500

    v45

--- Ejercicios/ejercicio 3/ViajeMasBarato.java
+++ Ejercicios/ejercicio 3/ViajeMasBarato.java
@@ -1,8 +1,11 @@
 package ejercicio 3;
+
 public class ViajeMasBarato {
 // Este método calcula la matriz de costos mínimos
+
 public static int[][] calcularCostosMinimos(int[][] T) {
     int n = T.length; // Número de embarcaderos
+
     int[][] C = new int[n][n]; // Matriz de costos mínimos
 
     // Paso 1: Copiamos los costos directos de T a C
@@ -18,10 +21,25 @@ public static int[][] calcularCostosMinimos(int[][] T) {
         }
     }
      // Paso 2: Buscamos si es más barato ir haciendo escalas
+     
      for (int i = n - 2; i >= 0; i--) { // Empezamos desde el penúltimo embarcadero
+
         for (int j = i + 1; j < n; j++) { // Solo miramos hacia adelante
+
             for (int k = i + 1; k < j; k++) { // k es el punto intermedio (escala)
+
                 // Solo si hay camino válido
-                
+                if (T[i][k] != Integer.MAX_VALUE && C[k][j] != Integer.MAX_VALUE) {
+                    int costoConEscala = T[i][k] + C[k][j];
+                    C[i][j] = Math.min(C[i][j], costoConEscala);
+                }
+            }
+        }
+    }
+
+    return C; // Devolvemos la matriz con los costos más bajos entre todos los puntos
+}
+}
+
 
 
